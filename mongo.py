d06d22e5eba64da7c2370201083df22add9f1c47
TaraRhoseyn <taragulwell@gmail.com>
2021-10-12 20:15:22 +0000

    Add mutliple items

--- mongo.py
+++ mongo.py
@@ -21,9 +21,26 @@ conn = mongo_connect(MONGO_URI)
 
 coll = conn[DATABASE][COLLECTION]
 
-new_doc = {"first": "douglas", "last": "adams", "dob": "11/03/1952", "hair_color": "grey", "occupation": "writer", "nationality": "british"}
+new_docs = [{
+    # multi docs = array of dictionaries
+    "first": "terry",
+    "last": "pratchett",
+    "dob": "28/04/1948",
+    "gender": "m",
+    "hair_color": "not much",
+    "occuptation": "writer",
+    "nationality": "british"
+}, {
+    "first": "george",
+    "last": "rr martin",
+    "dob": "02/09/1948",
+    "gender": "m",
+    "hair_color": "white",
+    "occupation": "writer",
+    "nationality": "american"
+}]
 
-coll.insert(new_doc)
+coll.insert_many(new_docs)
 
 documents = coll.find()
 # MongoDB object = cursor
